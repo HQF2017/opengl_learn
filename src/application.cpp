2cbe41da4f1d24c5de103169688827c473528cdb
hsir <2726804672@qq.com>
2023-10-11 23:22:15 +0800

    10.11 finish p8

--- src/application.cpp
+++ src/application.cpp
@@ -1,15 +1,94 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
-static unsigned int CompileShader(const std::string& source, unsigned int type) {
-    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
-    const char* src = source.c_str();
+class ShaderConfStr {
+public:
+    ShaderConfStr(std::string& shaderName, std::string& confStr)
+    {
+        _shaderName = shaderName;
+        _confStr = confStr;
+    }
+
+    const void printInfo() const
+    {
+        std::cout << _shaderName << " shader: \n" << _confStr << std::endl;
+    }
+    std::string _shaderName;
+    std::string _confStr;
+};
+
+static std::vector<ShaderConfStr> ParseShaderConf(const std::string& confPath) {
+    std::ifstream stream(confPath);
+    std::string line;
+    std::string shaderName;
+    std::string confStr;
+    std::vector<ShaderConfStr> shaderConfs;
+    static const std::string begineStr = "#Shader";
+    while (getline(stream, line))
+    {
+        size_t pos;
+        pos = line.find(begineStr);
+        if (pos != std::string::npos)
+        {
+            if (!shaderName.empty())
+            {
+                shaderConfs.push_back(ShaderConfStr(shaderName, confStr));
+            }
+            confStr = "";
+            shaderName = line.substr(pos + begineStr.length() + 1, line.length() - 1);
+        }
+        else
+        {
+            confStr += line + "\n";
+        }
+    }
+    if (!shaderName.empty())
+    {
+        shaderConfs.push_back(ShaderConfStr(shaderName, confStr));
+    }
+    return shaderConfs;
 }
 
-static int CreateShadere(const std::string& vertexShader, const std::string& fragmentShader) {
+static unsigned int CompileShader(const std::string& source, unsigned int type) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int res;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &res);
+    if (res == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "compile shader " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "failed" <<std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
     unsigned int program = glCreateProgram();
     unsigned int vs = CompileShader(vertexShader, GL_VERTEX_SHADER);
+    unsigned int fs = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 
 int main(void)
@@ -50,6 +129,26 @@ int main(void)
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    const std::string confPath = "res/shader.conf";
+    std::vector<ShaderConfStr> shaderConfStrs = ParseShaderConf(confPath);
+    std::string vertexShader;
+    std::string fragmentShader;
+    for (const auto shaderConfStr : shaderConfStrs)
+    {
+        shaderConfStr.printInfo();
+        if (shaderConfStr._shaderName == "vertex")
+        {
+            vertexShader = shaderConfStr._confStr;
+        }
+        else if (shaderConfStr._shaderName == "fragment")
+        {
+            fragmentShader = shaderConfStr._confStr;
+        }
+    }
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
